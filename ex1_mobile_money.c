532ab1f32003738ad3b5e53d134afd4b6410d13b
NaiwumbweAssumpta <2400808557@std.kyu.ac.ug>
2026-03-25 01:46:56 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,28 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: NAIWUMBWE ASSUMPTA */
+/* Student Number: 24/U/BIE/08557/PE */
 
 #include <stdio.h>
 
-int main(void)
-{
-    /* Write your code here */
+int main(void) {
+    float amount_to_send;
+    float fee_percent;
+    float fee_amount;
+    float total_amount_deducted;
 
+    printf("Enter amount to send: ");
+    scanf("%f", &amount_to_send);
+
+    printf("Enter transaction fee (%%): ");
+    scanf("%f", &fee_percent);
+
+    fee_amount    = (fee_percent / 100) * amount_to_send;
+    total_amount_deducted = amount_to_send + fee_amount;
+
+   	printf("\n----- TRANSACTION SUMMARY -----\n");
+    printf("Amount Sent:     %.2f UGX\n", amount_to_send);
+    printf("Fee:             %.2f UGX\n", fee_amount);
+    printf("Total Amount Deducted:  %.2f UGX\n", total_amount_deducted);
+    printf("--------------------------------\n");
+   
     return 0;
 }
