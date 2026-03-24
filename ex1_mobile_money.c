e42b486164135eba5eec4fc1775bfabd4d6d37fa
NaiwumbweAssumpta <2400808557@std.kyu.ac.ug>
2026-03-25 01:50:14 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -15,12 +15,12 @@ int main(void) {
     printf("Enter transaction fee (%%): ");
     scanf("%f", &fee_percent);
 
-    fee_amount    = (fee_percent / 100) * amount_to_send;
+    fee_amount = (fee_percent / 100) * amount_to_send;
     total_amount_deducted = amount_to_send + fee_amount;
 
    	printf("\n----- TRANSACTION SUMMARY -----\n");
-    printf("Amount Sent:     %.2f UGX\n", amount_to_send);
-    printf("Fee:             %.2f UGX\n", fee_amount);
+    printf("Amount Sent: %.2f UGX\n", amount_to_send);
+    printf("Transaction Fee: %.2f UGX\n", fee_amount);
     printf("Total Amount Deducted:  %.2f UGX\n", total_amount_deducted);
     printf("--------------------------------\n");
    
